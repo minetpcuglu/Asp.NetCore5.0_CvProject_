0511b9a5a98365ede2ad740c0f24c06bdfcad283
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-10-28 19:49:08 +0300

    28/10 ViewComponents Add

---
+++ Asp.NetCore5.0_CvProject_/ViewComponents/About/AboutImage.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_CvProject_.ViewComponents.About
+{
+    public class AboutImage:ViewComponent
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var Value = aboutManager.GetList();
+            return View(Value);
+        }
+    }
+}
